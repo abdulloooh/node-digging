7a3587f492816f68376f183e9c56791b1040b24b
abdulloooh <abdullahakinwumi@gmail.com>
2020-08-24 14:05:09 +0100

    grouped test for clarity

--- tests-demo/tests/lib.test.js
+++ tests-demo/tests/lib.test.js
@@ -1,15 +1,31 @@
 const lib = require("../lib");
 
-test("Absolute - Should return a positive number for positive input", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
-});
+// test("Absolute - Should return a positive number for positive input", () => {
+//   const result = lib.absolute(1);
+//   expect(result).toBe(1);
+// });
 
-test("Absolute - Should return a positive number for negative input", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
-test("Absolute - Should return a zero for other input", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+// test("Absolute - Should return a positive number for negative input", () => {
+//   const result = lib.absolute(-1);
+//   expect(result).toBe(1);
+// });
+// test("Absolute - Should return a zero for other input", () => {
+//   const result = lib.absolute(0);
+//   expect(result).toBe(0);
+// });
+
+describe("absolute", () => {
+  it("should return a positive number for positive input", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a positive number for negative input", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+  it("should return a zero for input zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
